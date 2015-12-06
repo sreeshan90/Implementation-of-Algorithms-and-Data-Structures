af7addec5d71f0d9cc43b3a0e32be59233af2b80
Sreesha <sreeshan90@gmail.com>
2015-12-05 23:06:20 -0600

    added main method

--- Misc/Fibonacii.java
+++ Misc/Fibonacii.java
@@ -1,37 +1,52 @@
-/** Implementation of generation of nth Fibonacii number
- *  @author Sreesha
+/**
+ * Solution to Swift Navigation Application Challenge
+ * 
+ * @author Sreesha Nagaraj
  */
 
+public class SwiftNavigationChallenge {
 
-public class Fibonacii {
+	/**
+	 * Method to check of input int is a Prime number
+	 * 
+	 * @param res
+	 *            : input num
+	 * @return : Boolean variable stating whether the input is a prime number or
+	 *         not
+	 */
+	public static boolean isPrime(long res) {
 
-	private static long startTime, endTime, elapsedTime;
-	private static int phase = 0;
+		// skip all even numbers and only try up to the square root of the
+		// number.
+		if (res > 2 && res % 2 == 0 || res == 1) {
 
-	public static int findPower(int x, int n, int p) {
-
-		if (n == 0) {
-			return 1;
-		} else if (n == 1) {
-			return x;
-		} else {
-
-			int temp = (findPower(x, n / 2, p));
-			if (n % 2 == 0) {
-
-				return temp * temp;
-			} else {
-
-				return temp * temp * x;
-			}
-
+			return false;
 		}
 
+		int top = (int) Math.sqrt(res) + 1;
+
+		for (int i = 2; i < top; i += 2) {
+			if (res % i == 0) {
+
+				return false;
+			}
+		}
+
+		return true;
 	}
 
-	public static long[][] findMatrixPower(long l, long p) {
+	/**
+	 * Method to calculate power of the matrix
+	 * 
+	 * @param num
+	 *            : int - power
+	 * @return : int[][] Matrix raised to given power
+	 * 
+	 */
 
-		if (l == 0) {
+	public static long[][] findMatrixPower(long pow) {
+
+		if (pow == 0) {
 			long[][] base = new long[2][2];
 			for (int i = 0; i < 2; i++) {
 				for (int j = 0; j < 2; j++) {
@@ -39,7 +54,7 @@ public class Fibonacii {
 				}
 			}
 			return base;
-		} else if (l == 1) {
+		} else if (pow == 1) {
 			long[][] base = new long[2][2];
 			base[0][0] = 1;
 			base[0][1] = 1;
@@ -50,21 +65,21 @@ public class Fibonacii {
 
 		{
 
-			if (l % 2 == 0) {
+			if (pow % 2 == 0) {
 
-				long temp[][] = (findMatrixPower(l / 2, p));
+				long temp[][] = (findMatrixPower(pow / 2));
 				long[][] c = new long[2][2];
 				long[][] result = new long[2][2];
 
 				for (int i = 0; i < 2; i++) {
 					for (int j = 0; j < 2; j++) {
 						for (int k = 0; k < 2; k++)
-							c[i][j] = (c[i][j] + (temp[i][k] * temp[k][j])) % p;
+							c[i][j] = (c[i][j] + (temp[i][k] * temp[k][j]));
 					}
 				}
 				return c;
 			} else {
-				long temp[][] = (findMatrixPower(l / 2, p));
+				long temp[][] = (findMatrixPower(pow / 2));
 				long[][] c = new long[2][2];
 				long[][] result = new long[2][2];
 				long[][] base = new long[2][2];
@@ -76,15 +91,15 @@ public class Fibonacii {
 				for (int i = 0; i < 2; i++) {
 					for (int j = 0; j < 2; j++) {
 						for (int k = 0; k < 2; k++)
-							c[i][j] = (c[i][j] + (temp[i][k] * temp[k][j])) % p;
+							c[i][j] = (c[i][j] + (temp[i][k] * temp[k][j]));
 					}
 				}
 
 				for (int i = 0; i < 2; i++) {
 					for (int j = 0; j < 2; j++) {
 						for (int k = 0; k < 2; k++)
-							result[i][j] = (result[i][j] + (c[i][k] * base[k][j]))
-									% p;
+							result[i][j] = (result[i][j] + (c[i][k] * base[k][j]));
+
 					}
 				}
 
@@ -95,68 +110,53 @@ public class Fibonacii {
 
 	}
 
-	public static long FiboRecursive(long n, int p) {
+	/**
+	 * Divide and Conquer method to calculate Fibonacii nummber
+	 * 
+	 * @param n
+	 *            : long - nth fibonacci number
+	 * @return : int[][] Matrix raised to given power
+	 * 
+	 */
 
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else if (n == 2) {
-			return 1;
-		} else {
-			return ((FiboRecursive((n - 1), p)) % p + (FiboRecursive((n - 2), p)) % p);
-		}
-	}
-	
-	public static long FiboIterative(long n,int p){
-		
-		 long prev1=0, prev2=1;
-         for(int i=0; i<n; i++) {
-             long savePrev1 = prev1;
-             prev1 = prev2 % p;
-             prev2 = (savePrev1 + prev2)% p;
-         }
-         return prev1 ;
-	}
+	public static long FiboDAC(long n) {
 
-	public static long FiboDAC(long n, int p) {
-
-		if (n == 0) {
-			return 0;
+		if (n == 0 || n < 0) {
+			return -1;
 		} else if (n == 1) {
 			return 0;
 		} else if (n == 2) {
 			return 1;
 		} else {
-			long[][] res = (findMatrixPower(n - 1, p));
+			long[][] res = (findMatrixPower(n - 1));
 
 			return res[0][0];
 		}
 	}
 
-	public static void timer() {
-		if (phase == 0) {
-			startTime = System.currentTimeMillis();
-			phase = 1;
-		} else {
-			endTime = System.currentTimeMillis();
-			elapsedTime = endTime - startTime;
-			phase = 0;
-		}
-	}
-
+	/**
+	 * Divide and Conquer method to calculate Fibonacii nummber
+	 * 
+	 * @param n
+	 *            : long - nth fibonacci number
+	 * @return : int[][] Matrix raised to given power
+	 * 
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		timer();
-		long res = FiboDAC(Integer.valueOf(args[0], Integer.valueOf(args[1]);
-		long time1 = elapsedTime;
-		timer();
-		timer();
-		long res2 = FiboIterative(Integer.valueOf(args[0], Integer.valueOf(args[1]);
-		timer();
-		long time2 = elapsedTime;
-		System.out.println(res + " "+" " + time2 + " " + time1);
+		long res = FiboDAC(Integer.valueOf(args[0]));
+
+		System.out.println(res);
+		if (res % 3 == 0) {
+			System.out.println("... Buzz");
+		} else if (res % 5 == 0) {
+			System.out.println("... Fizz");
+		} else if (isPrime(res)) {
+			System.out.println("... BuzzFizz");
+		} else {
+			System.out.println(res);
+		}
 
 	}
+
 }
